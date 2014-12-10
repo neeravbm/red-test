2715094c594a629f6df70a26a9feb991fdc5b9aa
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2014-12-10 11:43:54 +0530

    docker_functions.sh

---
+++ docker_functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. /var/lib/jenkins/portlookup/util_functions.sh
+create_container(){
+	container="${1}_Container"
+	host_sshport=$2
+	container_sshport=$3
+	host_vncport=$4
+	container_vncport=$5
+	host_nginxport=$6
+	container_nginxport=$7
+	image_name=$8
+	docker run -d --name $container -p ${host_sshport}:${container_sshport} -p ${host_vncport}:${container_vncport} -p ${host_nginxport}:${container_nginxport} ${image_name}
+}
+
