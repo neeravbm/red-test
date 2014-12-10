106ff75a7345074bf575928454c1fcdc038c2a27
Sandeep Rawat <sandeep_r80@yahoo.co.in>
2014-12-10 11:57:03 +0530

    Refactored jenkins functions.sh

--- jenkins_functions.sh
+++ jenkins_functions.sh
@@ -2,23 +2,30 @@
 
 source /var/lib/jenkins/JobScripts/shell_functions.sh
 
-create_new_job(){
-
+createJobConfigFromTemplate(){
     path_job_template=$1
-    feature_branch_name=$2
+    JOB_NAME=$2
 
-    new_job_name="${feature_branch_name}CodeQuality"
-    #Add a check if old job already exists or not
-    if [ -d "/var/lib/jenkins/jobs/${new_job_name}" ]; then
-        echo "This job already exists. Give another name."
-        exit 1
-    else
-        mkdir "/var/lib/jenkins/jobs/${new_job_name}"
-        cp "${path_job_template}" "/var/lib/jenkins/jobs/${new_job_name}/config.xml"
-
-	replaceTextInFile /var/lib/jenkins/jobs/${new_job_name}/config.xml {feature_branch} ${feature_branch_name}
-        echo " Restart jenkins server"
-	echo "A job create at /var/lib/jenkins/jobs/${new_job_name}/config.xml"
-    fi
+    cp "${path_job_template}" "/tmp/config.xml"
+    replaceTextInFile /tmp/config.xml {feature_branch} ${JOB_NAME}
 }
 
+createJob(){
+        JOB_NAME=$1
+        JOB_CONFIG=$2
+        JENKINS_CLI_JAR=/var/lib/jenkins/jenkins-cli.jar
+        JENKINS_URL=http://162.243.157.72:8080/
+
+        echo " INFO : Creating Job : $JOB_NAME "
+        java -jar ${JENKINS_CLI_JAR} -s $JENKINS_URL create-job ${JOB_NAME} < ${JOB_CONFIG}
+
+        export RC=$?
+        if [ $RC -ne 0 ]; then
+                echo " Error While Creating JOB : $JOB_NAME !!! "
+                echo " Failed with Error Code : $RC "
+                return 1
+        else
+                echo " INFO : Created Job : $JOB_NAME "
+                return 0
+        fi
+}
